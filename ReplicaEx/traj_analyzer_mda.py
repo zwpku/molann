ad0d6c598bc0c56f50cd07b4cd571c2821e78895
Wei Zhang <wzhangpku@gmail.com>
2021-12-23 16:23:30 +0100

    up

--- ReplicaEx/traj_analyzer_mda.py
+++ ReplicaEx/traj_analyzer_mda.py
@@ -20,39 +20,115 @@ import numpy as np
 import nglview as nv
 
 
-u = mda.Universe("./AlanineDipeptideOpenMM/vacuum.pdb", 'traj.dcd')
-ref = mda.Universe("./AlanineDipeptideOpenMM/vacuum.pdb")
-print (u.residues)
-print (u.trajectory)
-print (ref.trajectory)
+# ### Set parameters 
 
 # +
-nv.show_mdanalysis(u)
-res = u.residues[1]
+is_deactivate_warning = True
+is_savefigs = True
+is_makemovie = True
+is_makemovie_from_disk = False
 
-phi = res.phi_selection()
-res
-#protein = u.select_atoms('protein')
-#protein
+sysname = 'AlanineDipeptide'
+# name of PDB file
+pdb_filename = "./AlanineDipeptideOpenMM/vacuum.pdb"
+# name of DCD file
+traj_dcd_filename = 'traj.dcd'
+
+if is_deactivate_warning :
+    import warnings
+    warnings.filterwarnings("ignore")    
+    
+if is_makemovie :
+    import moviepy.editor as mpy
+    from time import sleep
+    # display the gif in this notebook
+    from IPython import display
+    
 # -
 
-r = dihedrals.Ramachandran(u.select_atoms('resid 2')).run()
-r.plot()
+# ### Load and display trajectory data 
 
+# +
+# load the trajectory data from DCD file
+u = mda.Universe(pdb_filename, traj_dcd_filename)
+# load the reference configuration from the PDB file
+ref = mda.Universe(pdb_filename) 
+
+# print some information
+print ('residues: ', u.residues)
+print ('trajectory: ', u.trajectory)
+print ('reference: ', ref.trajectory)
+
+# display the trajectory
+view = nv.show_mdanalysis(u)
+print ('number of frames: %d ' % view.max_frame)
+view
+# -
+
+# ### Generate plots
+
+# +
+# generate the Ramachandran plot of two dihedral angles
+ax = plt.gca()
+r = dihedrals.Ramachandran(u.select_atoms('resid 2')).run()
+r.plot(ax, color='black', marker='.') #, ref=True)
+
+if is_savefigs :
+    fig_filename_Ramachandran = '%s_Ramachandran.eps' % sysname
+    plt.savefig(fig_filename_Ramachandran)
+    print ('Ramachandran plot saved to file: %s' % fig_filename_Ramachandran)
+
+# +
+# select atoms for RMSD computation below 
 selector = 'name N or name CA or name C'
-#sel = u.select_atoms(selector)
-#print (sel)
+# compute RMSD of the trajectory wrt the reference configuration
 R = rms.RMSD(u, ref, select=selector)          
 R.run()
-
+# get the RMSD result
 rmsd = R.results.rmsd.T   # transpose makes it easier for plotting
-print (rmsd.shape)
-fig = plt.figure(figsize=(4,4))
+
 plt.plot(rmsd[0,:], rmsd[2,:], 'k-')
-#ax.legend(loc="best")
 plt.xlabel("time (fs)")
 plt.ylabel(r"RMSD ($\AA$)")
-#plt.xlim([0,2000])
-#fig.savefig("rmsd_all_CORE_LID_NMP_ref1AKE.pdf")
+
+if is_savefigs :
+    fig_filename_rmsd = '%s_rmsd.eps' % sysname 
+    plt.savefig(fig_filename_rmsd)    
+    print ('RMSD plot saved to file: %s' % fig_filename_rmsd)
+# -
+
+# ### Make a movie out of trajectory data (experimental)
+
+# +
+if is_makemovie :
+    n_frame = 10
+    download_dir = '~/Downloads'
+    if is_makemovie_from_disk :
+        # to save time for this tutorial, we make a movie with only a few frames
+        # this for loop will generate many png pictures.
+        for frame in range(0, n_frame):
+            # set frame to update coordinates
+            view.frame = frame
+            # make sure to let NGL spending enough time to update coordinates
+            sleep(0.5)
+            filename = 'tmp{}.png'.format(frame)
+            print ('frame %d saved to file: %s' % (frame, filename) )
+            view.download_image(filename=filename)
+            # make sure to let NGL spending enough time to render before going to next frame
+            sleep(2.0)
+
+    template = '%s/tmp{}.png' % download_dir
+    # get all (sorted) image files
+    imagefiles = [template.format(str(i)) for i in range(0, n_frame, 2)]
+    # make a gif file
+    frame_per_second = 8
+    im = mpy.ImageSequenceClip(imagefiles, fps=frame_per_second)
+    #image_file = '%s/tmp0.png' % download_dir
+    #im = mpy.ImageClip(image_file, duration=3)
+    #print (im.get_frame(3).shape)
+    im.write_gif('my_movie.gif', fps=frame_per_second)
+
+display.HTML("<img src='my_movie.gif'></img>")
+# -
 
 
