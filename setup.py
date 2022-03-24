747e15e3df88800083ad81af42e0d16ab2160af6
Wei Zhang <wzhangpku@gmail.com>
2022-03-24 22:18:53 +0100

    mv src/molcv to molcv

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
