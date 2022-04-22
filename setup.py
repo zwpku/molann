819529c4b34559aba97102bfebf1da0982b37da6
Wei Zhang <wzhangpku@gmail.com>
2022-04-22 15:45:00 +0200

    Remove essential setting from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,35 +1,7 @@
 #!/usr/bin/env python
-# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
 
-import setuptools
-import os
-
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
 if __name__ == '__main__':
-    setuptools.setup(
-        name="molann",
-        version="1.0.2",
-        author="Wei Zhang",
-        author_email="wzhangpku@gmail.com",
-        description="PyTorch Artificial Neural Networks (ANNs) for Molecular Systems",
-        long_description=long_description,
-        long_description_content_type="text/x-rst",
-        url="https://github.com/zwpku/molann",
-        project_urls={
-#        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-        },
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: Unix",
-        ],
-        packages=setuptools.find_packages(),
-        python_requires=">=3.7",
-    )
-
-
-
+    setup()
 
